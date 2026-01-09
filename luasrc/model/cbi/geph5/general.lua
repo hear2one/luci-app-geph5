bcb41415c50a65d46cd3ffb4e426a5f9d6e2b894
hors803 <107186496+hors803@users.noreply.github.com>
2026-01-09 20:27:35 +0800

    Update general.lua

--- luasrc/model/cbi/geph5/general.lua
+++ luasrc/model/cbi/geph5/general.lua
@@ -1,8 +1,8 @@
 local m, s, o
 
-m = Map("geph5", translate("Geph5 Client"), translate("Geph5 Client (Template Mode)"))
+m = Map("geph5", translate("Geph5 Client"), translate("Geph5 Client - Embedded Mode"))
 
--- 1. 运行状态栏
+-- 1. 运行状态
 s = m:section(TypedSection, "geph5", translate("Running Status"))
 s.anonymous = true
 s.template = "geph5/status"
@@ -12,31 +12,30 @@ s = m:section(TypedSection, "geph5", translate("Core Settings"))
 s.anonymous = true
 s.addremove = false
 
--- 启用开关
+-- 启用
 o = s:option(Flag, "enabled", translate("Enable"))
 o.rmempty = false
 
--- 程序路径
+-- 二进制路径
 o = s:option(Value, "binary_path", translate("Binary Path"))
 o.default = "/usr/bin/geph5-client"
 o.rmempty = false
 
--- 密钥输入框 (最重要的部分)
+-- 密钥输入框
 o = s:option(Value, "auth_token", translate("Auth Token / Secret"))
 o.password = true
 o.description = translate("Enter your 24-digit secret key here.")
 
--- SOCKS5 端口
+-- 端口设置
 o = s:option(Value, "socks5_port", translate("SOCKS5 Port"))
 o.datatype = "port"
 o.default = 9909
 
--- HTTP 端口
 o = s:option(Value, "http_port", translate("HTTP Port"))
 o.datatype = "port"
 o.default = 9919
 
--- 3. 日志查看器
+-- 3. 日志
 s = m:section(TypedSection, "geph5", translate("Log Viewer"))
 s.anonymous = true
 s.template = "geph5/log"
