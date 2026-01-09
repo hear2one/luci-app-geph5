a36b348d80fb3f4911b16bbf86ad48daf9081870
hors803 <107186496+hors803@users.noreply.github.com>
2026-01-09 18:52:22 +0800

    Update general.lua

--- luasrc/model/cbi/geph5/general.lua
+++ luasrc/model/cbi/geph5/general.lua
@@ -1,43 +1,42 @@
 local m, s, o
 
-m = Map("geph5", translate("Geph5 Client"), translate("Geph5 is a modular Internet censorship circumvention system."))
+m = Map("geph5", translate("Geph5 Client"), translate("Geph5 Client (Template Mode)"))
 
--- 【修正】这里改成了 geph5
+-- 1. 运行状态栏
 s = m:section(TypedSection, "geph5", translate("Running Status"))
 s.anonymous = true
 s.template = "geph5/status"
 
--- 【修正】这里改成了 geph5
-s = m:section(TypedSection, "geph5", translate("Basic Settings"))
+-- 2. 核心设置
+s = m:section(TypedSection, "geph5", translate("Core Settings"))
 s.anonymous = true
 s.addremove = false
 
+-- 启用开关
 o = s:option(Flag, "enabled", translate("Enable"))
 o.rmempty = false
 
+-- 程序路径
 o = s:option(Value, "binary_path", translate("Binary Path"))
 o.default = "/usr/bin/geph5-client"
 o.rmempty = false
 
-o = s:option(Value, "username", translate("Username"))
-o:depends("auth_token", "") 
-
-o = s:option(Value, "password", translate("Password"))
+-- 密钥输入框 (最重要的部分)
+o = s:option(Value, "auth_token", translate("Auth Token / Secret"))
 o.password = true
-o:depends("auth_token", "")
+o.description = translate("Enter your 24-digit secret key here.")
 
-o = s:option(Value, "auth_token", translate("Or: Auth Token / Key"))
-o.description = translate("If you have a direct key instead of user/pass.")
-
+-- SOCKS5 端口
 o = s:option(Value, "socks5_port", translate("SOCKS5 Port"))
 o.datatype = "port"
 o.default = 9909
 
+-- HTTP 端口
 o = s:option(Value, "http_port", translate("HTTP Port"))
 o.datatype = "port"
 o.default = 9919
 
--- 【修正】这里改成了 geph5
+-- 3. 日志查看器
 s = m:section(TypedSection, "geph5", translate("Log Viewer"))
 s.anonymous = true
 s.template = "geph5/log"
